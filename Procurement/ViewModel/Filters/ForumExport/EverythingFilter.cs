0ee6372361abb47a8ec265083468cf41d9960df7
theAprel <cpindziak@ufl.edu>
2016-06-12 21:31:40 -0400

    Make Procurement list everything

---
+++ Procurement/ViewModel/Filters/ForumExport/EverythingFilter.cs
@@ -0,0 +1,33 @@
+﻿using POEApi.Model;
+using System.Linq;
+
+namespace Procurement.ViewModel.Filters
+{
+    internal class EverythingFilter : IFilter
+    {
+        public FilterGroup Group
+        {
+            get { return FilterGroup.Default; }
+        }
+
+        public bool CanFormCategory
+        {
+            get { return true; }
+        }
+
+        public string Keyword
+        {
+            get { return "Everything"; }
+        }
+
+        public string Help
+        {
+            get { return "Everything"; }
+        }
+
+        public bool Applicable(POEApi.Model.Item item)
+        {
+			return true;
+        }
+    }
+}
---
+++ Procurement/ViewModel/ForumExportVisitors/EverythingVisitor.cs
@@ -0,0 +1,15 @@
+﻿using POEApi.Model;
+using Procurement.ViewModel.Filters.ForumExport;
+using System.Collections.Generic;
+
+namespace Procurement.ViewModel.ForumExportVisitors
+{
+    internal class EverythingVisitor : VisitorBase
+    {
+        private const string TOKEN = "{Everything}";
+        public override string Visit(IEnumerable<Item> items, string current)
+        {
+            return current.Replace(TOKEN, runFilter<Procurement.ViewModel.Filters.EverythingFilter>(items));
+        }
+    }
+}
